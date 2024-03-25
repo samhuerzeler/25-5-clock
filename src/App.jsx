64835ce5b99bfebf32efc86e16b2e2c816796550
samhuerzeler <samuelhuerzeler@gmail.com>
2024-03-25 16:21:05 +0100

    Fix build

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>25 + 5 Clock</h1>
     </>
   )
 }
